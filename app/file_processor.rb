afd9c8884b8319204c951a7592b2bbf3de8fa76f
Anika Bernstein <anikajb92@gmail.com>
2022-03-23 16:29:29 -0600

    Framework in place for activities to run in order

--- app/file_processor.rb
+++ app/file_processor.rb
@@ -1,5 +1,20 @@
 class File_Processor
   def run_process
-    puts "Running process"
+    puts 'Running process'
+    create_file
+    modify_file
+    delete_file
+  end
+
+  def create_file
+    puts 'creating file'
+  end
+
+  def modify_file
+    puts 'modifying file'
+  end
+
+  def delete_file
+    puts 'deleting file'
   end
 end
