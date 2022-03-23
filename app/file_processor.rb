39638995f620152eb7fa6caf8f2b0a00dedc6486
Anika Bernstein <anikajb92@gmail.com>
2022-03-23 16:40:51 -0600

    Log file created and capturing data from create_file

--- app/file_processor.rb
+++ app/file_processor.rb
@@ -10,6 +10,7 @@ class File_Processor
     puts 'creating file'
     cat_file = File.open('Cats.txt', 'a+')
     puts 'CREATED Cats.txt'
+    log_activity('create_file')
   end
 
   def modify_file
@@ -20,5 +21,15 @@ class File_Processor
 
   def delete_file
     puts 'deleting file...'
+  end
+
+  def log_activity(command)
+    activity_info = {
+      timestamp: DateTime.now.to_s,
+      activity_descriptor: command.to_s
+    }
+    log = File.open('Log.json', 'a+')
+    log.puts JSON.generate(activity_info)
+    puts "LOGGING #{command}"
   end
 end
