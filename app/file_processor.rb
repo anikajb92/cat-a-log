e5dba424c827cabd5fa27c76c61fd17e3edb28ec
Anika Bernstein <anikajb92@gmail.com>
2022-03-23 17:36:44 -0600

    Log capturing Process id numbers for run_process and its children's methods

--- app/file_processor.rb
+++ app/file_processor.rb
@@ -1,7 +1,8 @@
 class File_Processor
   def run_process
+    id = Process.pid
     puts 'Running file processor'
-    log_process
+    log_process id
     create_file
     sleep 2 # wait 2 seconds
     modify_file
@@ -19,46 +20,51 @@ class File_Processor
 
   # ACTIVITY METHODS
   def create_file
+    id = Process.pid
     if File.exist?('Cats.txt')
       puts 'Cats.txt file found...'
     else
       puts 'creating file'
       cat_file = File.open('Cats.txt', 'a+')
       puts 'CREATED Cats.txt'
-      log_activity('create_file')
+      log_activity('create_file', id)
     end
   end
 
   def modify_file
+    id = Process.pid
     puts 'modifying file'
     File.write('Cats.txt', "You've got to be kitten me", mode: 'a')
     puts 'MODIFIED Cats.txt'
-    log_activity('modify_file')
+    log_activity('modify_file', id)
   end
 
   def delete_file
+    id = Process.pid
     puts 'deleting file...'
     File.delete('Cats.txt')
     puts 'DELETED Cats.txt'
-    log_activity('delete_file')
+    log_activity('delete_file', id)
   end
 
   # LOG METHODS
-  def log_process
+  def log_process(id)
     process_info = {
       timestamp: current_time.to_s,
       username: $username.to_s,
-      process_name: 'run_process'
+      process_name: 'run_process',
+      process_id: id
     }
     $log.puts JSON.generate(process_info)
   end
 
-  def log_activity(command)
+  def log_activity(command, id)
     activity_info = {
       timestamp: current_time.to_s,
       activity_descriptor: command.to_s,
       username: $username.to_s,
-      file_path: $absolute_path
+      file_path: $absolute_path,
+      process_id: id
     }
     $log.puts JSON.generate(activity_info)
     puts "LOGGING #{command}"
