180df158e9dbee3cc0e2033af6d87cb65e60f505
Anika Bernstein <anikajb92@gmail.com>
2022-03-23 19:24:11 -0600

    localhost:9393/log displaying plain text

--- app/application.rb
+++ app/application.rb
@@ -2,10 +2,14 @@ require 'pry'
 require 'json'
 
 class Application
-
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
+
+    return index if req.path.match(/log/)
   end
 
+  def index
+    [200, { 'Content-Type' => 'text/plain' }, ['Hello, world']]
+  end
 end
