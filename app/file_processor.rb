6020198ace17889012edbea5588768de0e97c435
Anika Bernstein <anikajb92@gmail.com>
2022-03-28 19:20:18 -0600

    updated timezone and added kitty

--- app/file_processor.rb
+++ app/file_processor.rb
@@ -20,7 +20,7 @@ class File_Processor
   $command = Process.argv0
 
   def current_time
-    time = DateTime.now.strftime('%m-%d-%Y %H:%M:%S')
+    time = DateTime.now.strftime('%m-%d-%Y %H:%M:%S:%L %Z')
   end
 
   # ACTIVITY METHODS
@@ -31,7 +31,7 @@ class File_Processor
       puts 'creating file'
       cat_file = File.open('Cats.txt', 'a+')
       # Birthtime capturing the very first creation of file, regardless of how many times the program is run
-      birth_time = File.birthtime('Cats.txt').strftime('%m-%d-%Y %H:%M:%S')
+      birth_time = File.birthtime('Cats.txt').strftime('%m-%d-%Y %H:%M:%S:%L %Z')
       puts 'CREATED Cats.txt'
       log_activity('create_file', birth_time)
     end
@@ -47,7 +47,7 @@ class File_Processor
     ]
     File.write('Cats.txt', puns.sample(1), mode: 'a')
     # Ruby method capturing true file modification time rather than method run timestamp
-    mod_time = File.mtime('Cats.txt').strftime('%m-%d-%Y %H:%M:%S')
+    mod_time = File.mtime('Cats.txt').strftime('%m-%d-%Y %H:%M:%S:%L %Z')
     puts 'MODIFIED Cats.txt'
     log_activity('modify_file', mod_time)
   end
--- runner.rb
+++ runner.rb
@@ -1,6 +1,22 @@
 require_relative 'config/environment'
 
 $process = File_Processor.new
+$kitty = "
+                 .\": o :\':         ;\': o :\".
+                 \`. \`-\' .'.       .\'. \`-\' .'
+                   \`---'             \`---'
+
+         _...----...      ...   ...      ...----..._
+      .-\'__..-\"\"\'----    \`.  \`x\`  .\'    ----\'\"\"-..__\`-.
+     \'.-\'   _.--\"\"\"\'       \`-._.-'       \'\"\"\"--._   \`-.\`
+     \'  .-\"\'                  :                  \`\"-.  \`
+       \'   \`.      .       _.\'\"\'._       .       .\'   \`
+             \`.      \..,.-\'\"       \"\'-.,./       .\'
+               \`.                           .\'
+                 \`-._                   _.-'
+                     \`\"\'--...___...--\'\"\`
+
+"
 
 def prompt
   TTY::Prompt.new
@@ -9,6 +25,7 @@ end
 def initiate
   system('clear')
   puts 'Welcome to Cat-A-Log'
+  puts $kitty
   consent = prompt.yes?('Would you like to run this program now?')
 
   if consent
